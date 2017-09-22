069676f0d31b93d56ccae7b7aa384d7090c59f44
Susan Fowler <infopro94@gmail.com>
2017-09-22 16:43:41 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,10 @@
 # require libraries/modules here
+require 'nokogiri'
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+  
   # write your code here
 end
