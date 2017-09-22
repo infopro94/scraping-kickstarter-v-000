8cce5d05bd93321c3351cd897137dc522e8c35e7
Susan Fowler <infopro94@gmail.com>
2017-09-22 17:41:28 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,7 +23,7 @@ def create_project_hash
       :location => project.css("ul.project-meta").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
-    
+
   end
 
   projects
